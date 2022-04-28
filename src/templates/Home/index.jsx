32aff5c4ba9f45904dd3b76c5a3bfcdc182b2a6d
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-04-28 18:42:51 -0300

    fix: deleted wrong navbar

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import { Main } from '../../components/Main/index.jsx';
-import { NavBar } from '../../components/NavBar/index.jsx';
+import { Header } from '../../components/Header/index.jsx';
 
 export const Home = () => {
   return (
     <>
-      <NavBar />
+      <Header />
       <Main />
     </>
   );
