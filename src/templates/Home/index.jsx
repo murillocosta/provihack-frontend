786a8e817b3918c3047eca57f76c607ab7b4545c
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-04-28 18:18:14 -0300

    feat: menu burger responsiveness

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Main } from '../../components/Main/Main.jsx';
-import { NavBar } from '../../components/NavBar/index.jsx';
+import { Header } from '../../components/Header/index.jsx';
 
 export const Home = () => {
   return (
     <>
-      <NavBar />
+      <Header />
       <Main />
     </>
   );
