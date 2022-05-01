d0745af3bb7cb54c3077ab095017ca90af64ca82
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-05-01 11:36:50 -0300

    fix: correct logo added

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { NavBar, Logo } from './styles';
 import { Burger } from './Burger';
+import ecoParLogo from '../../img/ecoparlogo2.png'
 
 export const Nav = () => {
   return (
     <NavBar>
-      <Logo>EcoPartilhar</Logo>
+      <Logo><img src={ecoParLogo} /></Logo>
       <Burger />
     </NavBar>
   );
